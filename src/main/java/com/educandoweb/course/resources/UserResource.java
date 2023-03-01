e2b7203b7601b489852e7447eacbfb592c4cec4c
Taylan Bustamante <taylanbustamante@gmail.com>
2023-03-01 16:23:24 -0300

    User entity and resource (update)

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+
     @GetMapping
     public ResponseEntity<User> findAll() {
         User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
