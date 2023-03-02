3af94a22be05a7cb912fa75fcf38940efe08d224
Taylan Bustamante <taylanbustamante@gmail.com>
2023-03-02 16:16:19 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -14,12 +14,17 @@ public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
+
     private String phone;
+
     private String password;
 
     @JsonIgnore
